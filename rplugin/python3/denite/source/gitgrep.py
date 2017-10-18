e43bdd53bac29105efd029f9693941d0c452b61d
himanoa <matsunoappy@gmail.com>
2017-10-19 06:56:02 +0900

    Implement show git grep stdoutput

---
+++ rplugin/python3/denite/source/gitgrep.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from .base import Base
+import subprocess
+
+
+def run_command(command, cwd, encode='utf8'):
+    process = subprocess.run(command,
+                             cwd=cwd,
+                             stdout=subprocess.PIPE)
+
+    return process.stdout.decode(encode).split('\n')
+
+
+class Source(Base):
+
+    def __init__(self, vim):
+        super().__init__(vim)
+        self.vim = vim
+        self.name = 'git-grep'
+
+    def on_init(self, context):
+        pass
+
+    def on_close(self, context):
+        pass
+
+    def gather_candidates(self, context):
+        # command: git --no-pager grep -n --no-color
+        args = [x for x in ['git',
+                '--no-pager',
+                'grep',
+                '-n',
+                '--no-color',
+                ' '.join(context['args'][1::]),
+                '--',
+                context['args'][0]] if len(x) != 0]
+        return [{'word': word} for word in run_command(args, self.vim.eval('getcwd()'))]
